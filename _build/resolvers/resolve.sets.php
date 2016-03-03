5959fc8537e48f129ca87a71846d49c55a5237aa
Володя Володин <vovanblya@mail.ru>
2016-03-03 18:26:27 +0300

    1.0.2-beta ============== - Add PropertySet - Add bootstrap-material-design

---
+++ _build/resolvers/resolve.sets.php
@@ -0,0 +1,37 @@
+<?php
+/** @var $modx modX */
+if (!$modx = $object->xpdo AND !$object->xpdo instanceof modX) {
+    return true;
+}
+/** @var $options */
+switch ($options[xPDOTransport::PACKAGE_ACTION]) {
+    case xPDOTransport::ACTION_INSTALL:
+    case xPDOTransport::ACTION_UPGRADE:
+        foreach (array('pdoPage', 'pdoMenu') as $sname) {
+            /* @var modSnippet $snippet */
+            if (!$snippet = $modx->getObject('modSnippet', array('name' => $sname))) {
+                continue;
+            }
+            $sets = (array)include MODX_ASSETS_PATH . 'components/twiggybootstrap/elements/sets/set.' . strtolower($sname) . '.php';
+            if (empty($sets)) {
+                continue;
+            }
+            foreach ($sets as $name => $property) {
+                $name = $name . ucfirst($sname);
+                /** modPropertySet $propertySet */
+                if (!$propertySet = $modx->getObject('modPropertySet', array('name' => $name))) {
+                    $propertySet = $modx->newObject('modPropertySet');
+                }
+                $propertySet->set('properties', $property);
+                $propertySet->set('name', $name);
+                $propertySet->save();
+                if ($snippet->addPropertySet($propertySet)) {
+                    $snippet->save();
+                }
+            }
+        }
+        break;
+    case xPDOTransport::ACTION_UNINSTALL:
+        break;
+}
+return true;
